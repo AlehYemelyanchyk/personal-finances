5afb50cab1f1fea9fdb0e85957c128c851f2a2ab
Aleh Yemelyanchyk <yem1403@gmail.com>
2020-09-10 15:05:22 +0300

    Initial commit

---
+++ src/test/java/com/alehyem/personalfinances/PersonalFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alehyem.personalfinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
