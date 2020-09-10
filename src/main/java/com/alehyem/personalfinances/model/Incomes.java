154720e2fbdc69d01958d9acf367ca2e75f92c12
Aleh Yemelyanchyk <yem1403@gmail.com>
2020-09-10 16:17:57 +0300

    Adding Income POJO

---
+++ src/main/java/com/alehyem/personalfinances/model/Incomes.java
@@ -0,0 +1,42 @@
+package com.alehyem.personalfinances.model;
+
+import java.time.LocalDate;
+
+public class Incomes {
+    private String name;
+    private String currency;
+    private LocalDate date;
+    private double amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
